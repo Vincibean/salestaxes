35e0c66784cda491a4c0a15975971a26ce79c2a4
Vincibean <andrebessi00@gmail.com>
2015-05-26 23:44:47 +0200

    Added suooirt fir Category persistence

---
+++ src/main/java/org/vincibean/salestaxes/domain/Category.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2015  Vincibean <Andre Bessi>
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.vincibean.salestaxes.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import lombok.Data;
+
+/**
+ * A Java Bean representing a Category, a common type for poiuyt objects.
+ * Methods generated with Lombok.
+ * 
+ * @author Vincibean
+ *
+ */
+@Data
+@Entity
+@Table(name="CATEGORY")
+public class Category implements Serializable {
+
+	private static final long serialVersionUID = 3435562823660558022L;
+
+	/**
+	 * The unique identifier for this {@link Category}.
+	 */
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+
+	/**
+	 * The name of this {@link Category}.
+	 */
+	@Column(name="NAME")
+	@NotNull
+	private String name;
+
+	/**
+	 * A description for this {@link Category}.
+	 */
+	@Column(name="DESCRIPTION")
+	private String description;
+
+	/**
+	 * The {@link Fee} associated to this {@link Category}.
+	 */
+	@OneToOne (cascade=CascadeType.ALL)
+	@JoinColumn(name="FEE")
+	private Fee fee;
+
+}
---
+++ src/test/java/org/vincibean/salestaxes/domain/CategoryTest.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2015  Vincibean <Andre Bessi>
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.vincibean.salestaxes.domain;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+/**
+ * JUnit class covering methods of class org.vincibean.salestaxes.domain.Category
+ * 
+ * @author Vincibean
+ *
+ */
+public class CategoryTest {
+
+	/**
+	 * Data objects that will be used in test methods.
+	 */
+
+	private Category category;
+
+	/**
+	 * Set up all data objects that will be used in test methods.
+	 */
+	@Before
+	public void setUp(){
+		category = new Category();
+	}
+
+	/**
+	 * Test that setId()/getId() will behave consistently. 
+	 */
+	@Test
+	public void testId(){
+		long id = Long.MAX_VALUE;
+		category.setId(id);
+		Assert.assertEquals(id, category.getId().longValue());
+	}
+
+	/**
+	 * Test that setName()/getName() will behave consistently. 
+	 */
+	@Test
+	public void testName(){
+		String name = "JUnit";
+		category.setName(name);
+		Assert.assertEquals(name, category.getName());
+	}
+
+	/**
+	 * Test that setDescription()/getDescription() will behave consistently. 
+	 */
+	@Test
+	public void testDescription(){
+		String description = "JUnit Test Description";
+		category.setDescription(description);
+		Assert.assertEquals(description, category.getDescription());
+	}
+
+	/**
+	 * Test that setFee()/getFee() will behave consistently. 
+	 */
+	@Test
+	public void testFee(){
+		Fee mockFee = Mockito.mock(Fee.class);
+		category.setFee(mockFee);
+		Assert.assertEquals(mockFee, category.getFee());
+	}
+
+}
